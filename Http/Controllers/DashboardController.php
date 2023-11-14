c397487b0f65ac58f64d0f45115fe2de8ac10d83
Andressa <mariaandressamonteiro11@gmail.com>
2023-11-14 14:33:50 -0300

    subindo projeto

--- Http/Controllers/DashboardController.php
+++ Http/Controllers/DashboardController.php
@@ -3,11 +3,51 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Categoria;
+use App\Models\Produto;
+use DB;
 
 class DashboardController extends Controller
 {
 
     public function index() {
-        return view('admin.dashboard');
+
+        $usuarios = User::all()->count();
+        
+        // grafico 1 - usuarios
+        $usersData = User::select([
+            DB::raw('YEAR(created_at) as ano'),
+            DB::raw('COUNT(*) as total')
+    ])
+        ->groupBy('ano')
+        ->orderBy('ano', 'asc')
+        ->get();
+        
+        //preparar arrays
+        foreach($usersData as $user) {
+            $ano[] = $user->ano;
+            $total[] = $user->total;
+       }
+
+        //formatar para chart.js
+        $userLabel = "'Comparativo de cadastros de usuários'";
+        $userAno = implode(',', $ano);
+        $userTotal = implode(',', $total);
+
+        //grafico 2 - categorias
+        //$catData = Categoria::with('produtos')->get();
+
+        //preparar o array
+        //foreach($catData as $cat) {
+         // $catNome[] = "'".$cat->nome."'";
+          //$catTotal[] = $cat->produtos->count();
+        //}
+
+       // formatar para chart.js
+        //$catLabel = implode(',', $catNome);
+        //$catTotal = implode(',', $catTotal);
+
+        return view('admin.dashboard', compact('usuarios', 'userLabel', 'userAno', 'userTotal'));
     }
 }
--- Models/Produto.php
+++ Models/Produto.php
@@ -8,6 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Produto extends Model
 {
     use HasFactory;
+
+    protected $fillable =[
+        'nome',
+        'descricao',
+        'preco',
+        'imagem',
+        'slug',
+        'id_categoria',
+        'id_user',
+    ];
+
     protected $table = 'produtos';
 
     public function user() {
